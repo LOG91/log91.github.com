f2de39c61cdd85dec42bfe392af90d3f40a9b40b
Log91 <peter1636@naver.com>
2019-01-08 15:21:47 +0900

    modify: url 변경

--- bmc/js/Model/data/apiUrl.js
+++ bmc/js/Model/data/apiUrl.js
@@ -2,9 +2,14 @@
   배민찬 메뉴 API
  */
 export const apiUrl = {
-  bestMenu: "http://crong.codesquad.kr:8080/woowa/best",
-  mainMenu: "http://crong.codesquad.kr:8080/woowa/main",
-  sideMenu: "http://crong.codesquad.kr:8080/woowa/side",
-  courseMenu: "http://crong.codesquad.kr:8080/woowa/course",
-  autoComplete: "http://crong.codesquad.kr:8080/ac"
+  bestMenu:
+    "https://raw.githubusercontent.com/LOG91/log91.github.com/master/bmc/js/Model/data/best.json",
+  mainMenu:
+    "https://raw.githubusercontent.com/LOG91/log91.github.com/master/bmc/js/Model/data/main.json",
+  sideMenu:
+    "https://raw.githubusercontent.com/LOG91/log91.github.com/master/bmc/js/Model/data/side.json",
+  courseMenu:
+    "https://raw.githubusercontent.com/LOG91/log91.github.com/master/bmc/js/Model/data/course.json",
+  autoComplete:
+    "https://raw.githubusercontent.com/LOG91/log91.github.com/master/bmc/js/Model/data/ac"
 };
