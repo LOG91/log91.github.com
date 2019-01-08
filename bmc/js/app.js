143363ff5512ee68052e9521a01a24162655dd8d
Log91 <peter1636@naver.com>
2019-01-08 15:59:27 +0900

    test

--- bmc/js/app.js
+++ bmc/js/app.js
@@ -2,24 +2,26 @@
 
 import { navItemList } from "./Model/data/navData.js";
 import { apiUrl } from "./Model/data/apiUrl.js";
-import { slideMenuData } from './Model/data/slideMenuData.js';
-import LocalStorage from './Model/localStroage.js';
+import { slideMenuData } from "./Model/data/slideMenuData.js";
+import LocalStorage from "./Model/localStroage.js";
 
 import Header from "./View/Header.js";
 import BestMenu from "./View/BestMenu.js";
-import Promotion from './View/Promotion.js'
-import Slider from './View/Slider.js';
-import ScrollBtn from './View/ScrollBtn.js'
+import Promotion from "./View/Promotion.js";
+import Slider from "./View/Slider.js";
+import ScrollBtn from "./View/ScrollBtn.js";
 import AutoComplete from "./View/AutoComplete.js";
 
-
 import { bestMenuItemTpl } from "./templates/bestMenuTpl.js";
-import { slideTpl } from './templates/slideMenuTpl.js'
-import { scrollBtnTpl } from './templates/scrollBtnTpl.js'
-import { navTpl } from './templates/navTpl.js'
-import { autoCompleteItemTpl, recentDataTpl } from './templates/autoCompleteTpl.js'
+import { slideTpl } from "./templates/slideMenuTpl.js";
+import { scrollBtnTpl } from "./templates/scrollBtnTpl.js";
+import { navTpl } from "./templates/navTpl.js";
+import {
+  autoCompleteItemTpl,
+  recentDataTpl
+} from "./templates/autoCompleteTpl.js";
 
-import { RequestAnimations } from './Util/raf.js'
+import { RequestAnimations } from "./Util/raf.js";
 
 const header = new Header(navItemList, navTpl);
 const autoComplete = new AutoComplete({
@@ -30,19 +32,31 @@ const autoComplete = new AutoComplete({
 });
 const bestMenu = new BestMenu(apiUrl.bestMenu, bestMenuItemTpl);
 const promotion = new Promotion(RequestAnimations);
-const scrollBtn = new ScrollBtn(
-  { scrollBtnTpl: scrollBtnTpl, className: 'scroll_btn_box', speed: 1000 });
+const scrollBtn = new ScrollBtn({
+  scrollBtnTpl: scrollBtnTpl,
+  className: "scroll_btn_box",
+  speed: 1000
+});
 
 const sideMenuSlide = new Slider({
-  category: 'side', apiUrl: apiUrl.sideMenu, tpl: slideTpl, slideMenuData: slideMenuData
+  category: "side",
+  apiUrl: apiUrl.sideMenu,
+  tpl: slideTpl,
+  slideMenuData: slideMenuData
 });
 
 const mainMenuSlide = new Slider({
-  category: 'main', apiUrl: apiUrl.mainMenu, tpl: slideTpl, slideMenuData: slideMenuData
+  category: "main",
+  apiUrl: apiUrl.mainMenu,
+  tpl: slideTpl,
+  slideMenuData: slideMenuData
 });
 
 const courseMenuSlide = new Slider({
-  category: 'course', apiUrl: apiUrl.courseMenu, tpl: slideTpl, slideMenuData: slideMenuData
+  category: "course",
+  apiUrl: apiUrl.courseMenu,
+  tpl: slideTpl,
+  slideMenuData: slideMenuData
 });
 
 document.addEventListener("DOMContentLoaded", () => {
