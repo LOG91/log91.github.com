2f93bf900a85dd1416c885f7c5a98ab51a74199f
Log91 <peter1636@naver.com>
2019-01-08 16:20:06 +0900

    test

--- javascript-vm/js/view/machineView.js
+++ javascript-vm/js/view/machineView.js
@@ -2,7 +2,7 @@
   Machine 랜더링을 담당하는 파일
   초기 디스플레이, 이벤트 시에 화면 변화를 담당한다
 */
-import { Util } from "../Util/util.js";
+import { Util } from "../util/util.js";
 export default class MachineView {
   constructor(template) {
     this.clickItemNumberButton = null;
